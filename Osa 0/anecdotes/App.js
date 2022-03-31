f33aae36203600b0bdd6f4f1c1a8f232acd8dae8
Tomppeli86 <99166139+Tomppeli86@users.noreply.github.com>
2022-03-31 14:57:17 +0300

    Update App.js

--- Osa 0/anecdotes/App.js
+++ Osa 0/anecdotes/App.js
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 
-
-
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
+
 
 const App = () => {
   
@@ -20,35 +18,50 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-
-  const points = []
-
-  const copy = []
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
    
-  const [selected, setSelected] = useState(0)
-  const [voted, setVoted] = useState(0)
-  
-  
   const getSelected = () => {
+
     const nro = Math.floor(Math.random() * 7);
-      if (setSelected() == nro) {
-        const nro2 = Math.floor(Math.random() * 7);
-          nro2 = nro
+      
+        if (setSelected() == nro) {
+          const nro2 = Math.floor(Math.random() * 7);
+            nro2 = nro
         }       
         if (setSelected() == nro) {
           const nro3 = Math.floor(Math.random() * 7);
             nro3 = nro
         }    
-  
     setSelected(nro);  
   }
 
-  const getVoted = () => {
-    
-    setVoted (copy[setSelected] += 1);
 
+  const getVoted = ()  => {
+
+    const copy = {...votes}
+    copy[selected] +=1
+    setVotes(copy)
   }
+
+
+  const findMax = () => {
+    let max = 0
+    let index = NaN
+
+    const compare = (val, ind) => {
+      if(val > max){
+        max = val;
+        index = ind
+      }
+    }
+    votes.forEach(compare)
+    return index
+  }
+
+  let top = findMax()
 
   return (
     
@@ -57,11 +70,13 @@ const App = () => {
         <h2>{anecdotes[selected]}</h2>
         
         <br></br><br></br><br></br>
-        <button onClick={getSelected}>Next anecdote</button>
+        <button onClick={getSelected}>Moarboar wants moar</button>
+        <p>Rating: {votes[selected]}</p>
         <button onClick={getVoted}>vote</button>
 
-        <h1>Anecdote with most votes</h1>
-        <h2>{anecdotes[voted]}</h2>
+        <h1>The top dog</h1>
+        <h2>{anecdotes[top]}</h2>
+        <p>Rating: {votes[top]}</p>
     </div>
   )
 }
