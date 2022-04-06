203b4c0f56dc8f153fccaa58fa7e505bc9125803
Tomppeli86 <99166139+Tomppeli86@users.noreply.github.com>
2022-04-06 14:02:08 +0300

    Add files via upload

--- Osa 2/Phonebook/App.js
+++ Osa 2/Phonebook/App.js
@@ -2,16 +2,16 @@ import { useState } from 'react'
 
 
 const App = () => {
+  
 
   const [persons, setPersons] = useState( [
       { name: 'Arto Hellas',
         number: '666',
-        id: 0 }
-  ] ) 
-
+        id: 0} ] ) 
+   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
- 
+  const [newSearch, setNewSearch] = useState('')
   
   const handleName = (event)  => {
     console.log(event.target.value)
@@ -19,48 +19,94 @@ const App = () => {
   }
   const handleNumber = (event)  => {
     console.log(event.target.value)
-    setNewName(event.target.value)
+    setNewNumber(event.target.value)
   }
+
+
+  const handleSearch = (event)  => {
+    setNewSearch(event.target.value)
+    const keyword = (event.target.value)
+    const copylist = [...persons];
+    
+    if (keyword !== '') {
+      const results = persons.filter((user) => {
+        return user.name.toLowerCase().startsWith(keyword.toLowerCase());  
+          });
+          setPersons(results)
+          console.log(results)
+        }
+      console.log('show allllll')
+      copylist = persons;
+      
+    }
+    
 
   const clickName = (event)  => {
     event.preventDefault()
+
+    if (newName.length === 0 || newNumber.length === 0) {
+      alert("Can't have empty fields")
+      return
+    }
     console.log("name created", newName)
-    console.log("name created", newNumber)
+    console.log("number created", newNumber)
     console.log('button clicked', event.target)
+
+    const newPersons = { name: newName, number: newNumber, id: parseInt(persons.lenght + 1)}
+    let existingNumber = persons.filter(person => person.number === newPersons.number)[0]
     
-
-    setNewName('')
+    if (existingNumber) {
+      console.log('exists')
+      
+      alert("Number is on the list")
+        } else {
+          setPersons(persons.concat(newPersons))
+        }
+        setNewName('')
+        setNewNumber('')
   }
-
+  
   return (
     <div>
+      <h2>Thick phonebook</h2>
+        <form>
+        <div>
+          Show: <input value={newSearch} type="text" name="search"
+              onChange={handleSearch}/>
+        </div>
+      </form>
       
-      <h2>Phonebook</h2>
-      
-      <div >Name: &nbsp; &nbsp; 
+
+      <h3>Add a new thang.</h3>
+
+      <form onSubmit={clickName}>
+      <label>Name: &nbsp; &nbsp; 
         <input value={newName} type="text" name="name"
               onChange={handleName}/>
               <br></br> 
-              </div>
+              </label>
 
               <br></br>
               
-              <div>Number:
+              <label>Number:
         <input value={newNumber} type="text" name="number"
               onChange={handleNumber}/>
               <br></br>
-              </div>
+              </label>
 
-      <button type="submit" onClick={clickName}>Add</button>
+      <button type="submit">Add</button>
+      </form>
       
 
-        <h2>Numbers</h2>
+        <h3>Numbas on da book</h3>
         
-        <div>
-        {persons.map(function(d, idx){
-         return (<li key={idx}>{d.name}&nbsp;{d.number}</li>)
-        })}
-        </div>
+        <div className="user-list">
+          <ol>
+        {persons.map((person) => (
+          <li>{person.name}{person.number}</li>
+          ))}
+        </ol>
+      </div>
 
     </div>
   );
