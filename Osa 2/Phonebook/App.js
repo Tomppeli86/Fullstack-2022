060e01d5643d8deb53b701db68bda102f717bca6
Tomppeli86 <99166139+Tomppeli86@users.noreply.github.com>
2022-04-07 14:40:11 +0300

    Add files via upload

--- Osa 2/Phonebook/App.js
+++ Osa 2/Phonebook/App.js
@@ -1,114 +1,119 @@
 import { useState } from 'react'
 
-
 const App = () => {
   
+  const [persons, setPersons] = useState( [  ] ) 
 
-  const [persons, setPersons] = useState( [
-      { name: 'Arto Hellas',
-        number: '666',
-        id: 0} ] ) 
-   
+  const [personsToShow, setPersonsToShow] = useState( [ 
+    { name: 'Example', number: '1234567', id: 1 }
+    ] )
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   
+  var clickCounter = 0;
+
   const handleName = (event)  => {
-    console.log(event.target.value)
+    setNewSearch('')
     setNewName(event.target.value)
   }
   const handleNumber = (event)  => {
-    console.log(event.target.value)
+    setNewSearch('')
     setNewNumber(event.target.value)
   }
 
-
-  const handleSearch = (event)  => {
+  const handleSearch = (event)  => { 
     setNewSearch(event.target.value)
     const keyword = (event.target.value)
-    const copylist = [...persons];
-    
-    if (keyword !== '') {
-      const results = persons.filter((user) => {
-        return user.name.toLowerCase().startsWith(keyword.toLowerCase());  
+    console.log(personsToShow)
+
+      if (keyword !== '') {
+          const results = persons.filter((user) => {
+          return user.name.toLowerCase().startsWith(keyword.toLowerCase());  
           });
-          setPersons(results)
-          console.log(results)
-        }
-      console.log('show allllll')
-      copylist = persons;
-      
-    }
+          setPersonsToShow(results)
+
+      } else {
+          setPersonsToShow(persons)
+      }
+  }
     
 
   const clickName = (event)  => {
-    event.preventDefault()
-
-    if (newName.length === 0 || newNumber.length === 0) {
-      alert("Can't have empty fields")
-      return
-    }
-    console.log("name created", newName)
-    console.log("number created", newNumber)
-    console.log('button clicked', event.target)
-
-    const newPersons = { name: newName, number: newNumber, id: parseInt(persons.lenght + 1)}
-    let existingNumber = persons.filter(person => person.number === newPersons.number)[0]
-    
-    if (existingNumber) {
-      console.log('exists')
+      event.preventDefault()
+      console.log(clickCounter)
       
-      alert("Number is on the list")
+        if (newName.length === 0 || newNumber.length === 0) {
+            alert("Can't have empty fields")
+            return  }
+
+      const newPersons = { name: newName, number: newNumber, id: parseInt(persons.lenght + 1)}
+      let existingNumber = persons.filter(person => person.number === newPersons.number)[0]
+    
+        if (existingNumber) {
+            alert("Number is already on the book")
+
         } else {
-          setPersons(persons.concat(newPersons))
+            setPersons(persons.concat(newPersons))
+            setPersonsToShow(persons.concat(newPersons))
+
+            clickCounter = clickCounter + 1;
+            console.log(clickCounter)
         }
-        setNewName('')
-        setNewNumber('')
+      setNewName('')
+      setNewNumber('')
   }
   
+
   return (
     <div>
-      <h2>Thick phonebook</h2>
-        <form>
-        <div>
-          Show: <input value={newSearch} type="text" name="search"
+      <h2>The phonebook</h2>
+        <form>  
+          <div>Show:&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+            <input 
+              value={newSearch} 
+              type="text" 
+              name="searchfield"
               onChange={handleSearch}/>
-        </div>
-      </form>
-      
-
-      <h3>Add a new thang.</h3>
-
-      <form onSubmit={clickName}>
-      <label>Name: &nbsp; &nbsp; 
-        <input value={newName} type="text" name="name"
+            </div>  
+        </form>
+    
+      <h3>Add a new person</h3>
+        <form onSubmit={clickName}>
+          <label>Name: &nbsp; &nbsp; &nbsp; &nbsp;
+            <input
+              key="namekey"
+              value={newName} 
+              type="text" 
+              name="namefield"
               onChange={handleName}/>
               <br></br> 
-              </label>
-
-              <br></br>
-              
-              <label>Number:
-        <input value={newNumber} type="text" name="number"
+          </label>
+                  <br></br>     
+          <label>Number: &nbsp; &nbsp;
+            <input
+              key="phonenumber"
+              value={newNumber} 
+              type="text" 
+              name="numberfield"
               onChange={handleNumber}/>
               <br></br>
-              </label>
+          </label>
+          <button type="submit">Add</button>  
+        </form> 
 
-      <button type="submit">Add</button>
-      </form>
-      
-
-        <h3>Numbas on da book</h3>
-        
-        <div className="user-list">
-          <ol>
-        {persons.map((person) => (
-          <li>{person.name}{person.number}</li>
-          ))}
-        </ol>
+      <h3>Numbers on the book</h3>
+      <div> 
+        <ol> <p>Name &nbsp; &nbsp; &nbsp; &nbsp; Number</p>
+          {personsToShow.map((person) => (
+            <li key={person.id}>
+                {person.name} &nbsp;- &nbsp; 
+                {person.number}
+            </li>))}
+        </ol> 
       </div>
-
-    </div>
+    </div> 
   );
 }
 
