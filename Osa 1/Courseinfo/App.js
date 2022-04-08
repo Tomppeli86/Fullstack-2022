0aa6996cd2c6bb1efd5eaa2095a2e5e4dff59439
Tomppeli86 <99166139+Tomppeli86@users.noreply.github.com>
2022-04-08 08:49:40 +0300

    Add files via upload

---
+++ Osa 1/Courseinfo/App.js
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return (
+    <h1>{props.coursename}</h1>
+  )
+}
+const Part = ({name,ex}) => {
+  return (
+    <p>{name} - {ex}</p>
+  )
+}
+const Content = ({parts}) => {
+  const [part1,part2,part3]=parts;
+  return (
+    <div>
+    <Part name = {part1.name} ex={part1.exercises}/>
+    <Part name = {part2.name} ex={part2.exercises}/>
+    <Part name = {part3.name} ex={part3.exercises}/>
+    </div>
+  )
+}
+const Total = ({parts}) => {
+  const [part1,part2,part3]=parts;
+  return (
+    <p>Number of exercises - {part1.exercises+part2.exercises+part3.exercises}</p>
+  )
+}
+const App = () => {
+  const course = {
+   name : "Half Stack application development",
+   parts : [
+    {
+    name: "Fundamentals of React",
+    exercises: 10
+  },
+  {
+    name: "Using props to pass data",
+    exercises: 7
+  },
+  {
+    name: "State of a component",
+    exercises: 14
+  }
+  ]
+}
+
+  return (
+    <div>
+      <Header coursename={course.name}/>
+      <Content parts={course.parts}  />
+      <Total parts={course.parts}    />
+    </div>
+  )
+}
+
+export default App;
