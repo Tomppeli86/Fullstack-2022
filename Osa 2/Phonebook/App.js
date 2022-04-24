936e447932e814e283166278c33926a2a3427142
Tomppeli86 <99166139+Tomppeli86@users.noreply.github.com>
2022-04-24 11:43:53 +0300

    3/3

--- Osa 2/Phonebook/App.js
+++ Osa 2/Phonebook/App.js
@@ -2,10 +2,8 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-
 const App = () => {
 
-  
   const [persons, setPersons] = useState( [  ] ) 
   const [personsToShow, setPersonsToShow] = useState( [ { 
     name: 'Example', number: '0123456789', id: 1
@@ -14,8 +12,6 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
-
-
 
   const handleName = (event)  => {
     setNewSearch('')
@@ -26,164 +22,175 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-
   const handleSearch = (event)  => { 
     setNewSearch(event.target.value)
     const keyword = (event.target.value)
-    console.log(personsToShow)
 
       if (keyword !== '') {
         const results = persons.filter((user) => {
-          return user.name.toLowerCase().startsWith(keyword.toLowerCase());
-            } );
+        return user.name.toLowerCase().startsWith(keyword.toLowerCase());}
+      )
           setPersonsToShow(results)
-
       } else {
           setPersonsToShow(persons)
-      }
+      } 
   }
 
 
   const hook = () => {
-
     console.log('effect')
-      axios
-        .get('http://localhost:3001/persons')
-        .then(response => {
-            console.log('promise fulfilled')
-            setPersons(response.data)
-        })
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setPersonsToShow(response.data)
+     })
   }
   useEffect(hook, [])
 
 
 
-
-
-
   const deleteNumber = (event) => {
 
-      event.preventDefault()
-      
-      let removeIndex = event.target.value
-      const deletePerson =  personsToShow.find( ({ id }) => id === parseInt(removeIndex) );
+    event.preventDefault()
+    let idnumber = event.target.value
 
-        if (window.confirm(`Delete number?`))  {
+    if (window.confirm(`Delete number?`)) {
+      const deletePerson = persons.findIndex( ({ id }) => id === parseInt(idnumber));
+      let stringDel = deletePerson.toString()
+        console.log(idnumber)
+        console.log(deletePerson)
 
+      axios
+        .delete('http://localhost:3001/persons/'+(idnumber))
+        .then(response => {
+          console.log(response) 
+          { 
           axios
-            .delete('http://localhost:3001/persons/'+(personsToShow.indexOf(deletePerson)))
+            .get('http://localhost:3001/persons')
             .then(response => {
-                console.log(response)
-              })
-
-            personsToShow.splice(personsToShow.indexOf(deletePerson),1);
-            setPersonsToShow(persons)
-              console.log(personsToShow)
-              console.log(persons)
-              console.log(personsToShow.indexOf(deletePerson))
-
-        } else {
-            console.log("return") 
-          return
-      }
+              console.log('promise fulfilled')
+              setPersons(response.data)
+              setPersonsToShow(response.data)
+          }) 
+        }
+      })
+    }
   }
 
 
-
-
-
-
+  
   const clickName = (event)  => {
 
     event.preventDefault()
-      
+    const numberId = Number.parseInt(newNumber)
+
       if (newName.length === 0 || newNumber.length === 0) {
           alert("Can't have empty fields")
-        return  }
-    
-    const newPersons = { name: newName, number: newNumber, id: parseInt(persons.length + 1)}
+          return  }
+      if (Number.isInteger(numberId)) {
+          console.log("is number, is niiiice")
+      } else {
+          alert("Only numbers between 0-9 are allowed as phonenumbers") 
+          return  }
+
+    const newPersons = { name: newName, number: newNumber, id: parseInt(numberId)}
     let existingNumber = persons.filter(person => person.number === newPersons.number)[0]
     let existingId = persons.filter(person => person.id === newPersons.id)[0]
+    let existingName = persons.filter(person => person.name === newPersons.name)[0]
+    console.log(newPersons.name)
 
       if (existingNumber) {
           alert("Number is already on the book")
-        return  }
+          return  }
+      if (existingId) {
+          alert("Duplicate I.D, ID = Phonenumber")
+          newPersons.id = persons.lenght + 10
+      }
 
-            if (existingId) {
-              const newPersons2 = { name: newName, number: newNumber, id: parseInt(persons.length + 1 + ({number: Math.random()}))}
-              console.log("RNG ID")
-                setPersons(persons.concat(newPersons2))
-                setPersonsToShow(persons.concat(newPersons2))
-            } else {
-                setPersons(persons.concat(newPersons))
-                setPersonsToShow(persons.concat(newPersons))
-            }
+      if (existingName) {
+          console.log(newPersons.id)
+          console.log(existingName.id)
+          (window.confirm(newPersons.name + ` is already on the phonebook, replace old number with new one?`)) 
+            axios
+              .put('http://localhost:3001/persons/'+(existingName.id),newPersons)
+              .then(response => {
+                console.log(response)
+                {
+                axios
+                  .get('http://localhost:3001/persons')
+                  .then(response => {
+                    console.log('promise fulfilled')
+                    setPersons(response.data)
+                    setPersonsToShow(response.data)
+                  }) 
+                }
+              })
+          setNewName('')
+          setNewNumber('')
+        return
 
+      } else {
         axios
-          .post('http://localhost:3001/persons', newPersons)
-          .then(response => {
-            console.log(response)
+          .post('http://localhost:3001/persons',newPersons)
+          .then(response => { 
+            console.log(response.data)
+            setPersons(persons.concat(newPersons))
+            setPersonsToShow(persons.concat(newPersons))
           })
-
+        }
     setNewName('')
     setNewNumber('')
   }
 
-  
+
   return (
 
     <div>
-      <h2>The phonebook</h2>
-        <form>  
-          <div>Show:&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
-            <input 
-              value={newSearch} 
+        <h2>The phonebook</h2>
+        <form> <div>Show:&nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+            <input value={newSearch} 
               type="text" 
               name="searchfield"
-              onChange={handleSearch}/>
-            </div>  
+              onChange={handleSearch}/></div> 
         </form>
     
 
-      <h3>Add a new person</h3>
-        <form onSubmit={clickName}>
+          <h3>Add a new person</h3>
+          <form onSubmit={clickName}>
+              <label>Name: &nbsp; &nbsp; &nbsp; &nbsp;
+              <input key="namekey"
+                 value={newName} 
+                 type="text" 
+                 name="namefield"
+                 onChange={handleName}/>
+                 <br></br> </label> <br></br>
+              <label>Number: &nbsp; &nbsp;
+              <input key="phonenumber"
+                  value={newNumber} 
+                  type="text" 
+                  name="numberfield"
+                  onChange={handleNumber}/>
+                  <br></br><br></br> </label>
+                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; 
+                  &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+            <button type="submit"> Add</button>  
+            </form> 
 
-          <label>Name: &nbsp; &nbsp; &nbsp; &nbsp;
-            <input
-              key="namekey"
-              value={newName} 
-              type="text" 
-              name="namefield"
-              onChange={handleName}/>
-              <br></br> 
-          </label> <br></br>
 
-          <label>Number: &nbsp; &nbsp;
-            <input
-              key="phonenumber"
-              value={newNumber} 
-              type="text" 
-              name="numberfield"
-              onChange={handleNumber}/>
-              <br></br><br></br>
-          </label>
-          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
-          <button type="submit">Add</button>  
-        </form> 
-
-        <br></br>
-      <h3>Numbers in the book</h3>
-        <div> 
-          <ol> <p>Name &nbsp; &nbsp; &nbsp; &nbsp; Number</p>
-            {personsToShow.map((person) => (
-              <li key={person.number}>
+          <br></br><h3>Numbers in the book</h3><div> <ol> 
+          <p>Name &nbsp; &nbsp; &nbsp; &nbsp; Number</p>
+              {personsToShow.map((person) => (
+                <li key={person.number}>
                 {person.name} &nbsp;- &nbsp; 
-                {person.number} &nbsp; &nbsp;<button type="button" value={person.id} onClick={deleteNumber}>Delete</button><br></br>
-              </li>))}
-          </ol> 
-        </div>
+                {person.number} &nbsp; &nbsp;
+                <button type="button" 
+                value={person.id} onClick={deleteNumber}>
+              Delete</button><br></br> </li>))}</ol> 
+          </div>
     </div> 
+
   );
 }
-
 export default App
